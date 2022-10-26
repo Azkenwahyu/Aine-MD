1024755bb85589bc140ecc49696425df58ada007
Azkenwahyu <109403067+Azkenwahyu@users.noreply.github.com>
2022-10-26 20:29:41 +0700

    Config

--- config.js
+++ config.js
@@ -2,9 +2,9 @@ let fs = require('fs')
 let chalk = require('chalk')
 
 owner = [
-  ['62895330379186'],
-  ['62895330379186'],
-  ['62895330379186', 'Owner', true]
+  ['6281333791513'],
+  ['6281333791513'],
+  ['6281333791513', 'Owner', azken]
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
 mods = [] // Want some help?
